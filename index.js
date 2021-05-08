2be5188d79db3d66843e557849e3d2a5eb47f291
penzai <pengzhen058@qq.com>
2021-05-08 09:29:03 +0800

    update

--- index.js
+++ index.js
@@ -1,148 +1,27 @@
-import {earcut} from '../common/lib/earcut.js';
-
-const canvas = document.querySelector('canvas');
-const gl = canvas.getContext('webgl');
-
-// 定点着色器
-const vertex = `
-
-attribute vec2 position;
-
-uniform float u_rotation;
-uniform float u_time;
-uniform float u_duration;
-uniform float u_scale;
-uniform vec2 u_dir;
-
-varying float vP;
-
-void main() {
-  float p = min(1.0, u_time / u_duration);
-  float rad = u_rotation + 3.14 * 10.0 * p;
-  float scale = u_scale * p * (2.0 - p);
-  vec2 offset = 2.0 * u_dir * p * p;
-  mat3 translateMatrix = mat3(
-    1.0, 0.0, 0.0,
-    0.0, 1.0, 0.0,
-    offset.x, offset.y, 1.0
-  );
-  mat3 rotateMatrix = mat3(
-    cos(rad), sin(rad), 0.0,
-    -sin(rad), cos(rad), 0.0,
-    0.0, 0.0, 1.0
-  );
-  mat3 scaleMatrix = mat3(
-    scale, 0.0, 0.0,
-    0.0, scale, 0.0,
-    0.0, 0.0, 1.0
-  );
-  gl_PointSize = 1.0;
-  vec3 pos = translateMatrix * rotateMatrix * scaleMatrix * vec3(position, 1.0);
-  gl_Position = vec4(pos, 1.0);
-  vP = p;
-}
-`;
-
-// 片元着色器
-const fragment = `
-precision mediump float;
-uniform vec4 u_color;
-varying float vP;
-
-void main()
-{
-  gl_FragColor.xyz = u_color.xyz;
-  gl_FragColor.a = (1.0 - vP) * u_color.a;
-}    
-`;
-
-const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertexShader, vertex);
-gl.compileShader(vertexShader);
-
-const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragmentShader, fragment);
-gl.compileShader(fragmentShader);
-
-
-const program = gl.createProgram();
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-gl.linkProgram(program);
-gl.useProgram(program);
-
-
-// 创建三角形
-const position = new Float32Array([ -1, -1, 0, 1, 1, -1,]);
-const bufferId = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, bufferId)
-gl.bufferData(gl.ARRAY_BUFFER, position, gl.STATIC_DRAW)
-
-const vPosition = gl.getAttribLocation(program, 'position')
-gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0)
-gl.enableVertexAttribArray(vPosition)
-
-
-// 创建随机三角形
-
-function randomTriangles() {
-  const u_color = [Math.random(), Math.random(), Math.random(), 1.0]; // 随机颜色
-  const u_rotation = Math.random() * Math.PI; // 初始旋转角度
-  const u_scale = Math.random() * 0.05 + 0.03; // 初始大小
-  const u_time = 0;
-  const u_duration = 3.0; // 持续3秒钟
-
-  const rad = Math.random() * Math.PI * 2;
-  const u_dir = [Math.cos(rad), Math.sin(rad)]; // 运动方向
-  const startTime = performance.now();
-
-  return {u_color, u_rotation, u_scale, u_time, u_duration, u_dir, startTime};
-}
-
-// 将随机三角形信息传给shader里的uniform变量
-
-function setUniforms(gl, {u_color, u_rotation, u_scale, u_time, u_duration, u_dir}) {
-  // gl.getUniformLocation 拿到uniform变量的指针
-  let loc = gl.getUniformLocation(program, 'u_color');
-  // 将数据传给 unfirom 变量的地址
-  gl.uniform4fv(loc, u_color);
-
-  loc = gl.getUniformLocation(program, 'u_rotation');
-  gl.uniform1f(loc, u_rotation);
-
-  loc = gl.getUniformLocation(program, 'u_scale');
-  gl.uniform1f(loc, u_scale);
-
-  loc = gl.getUniformLocation(program, 'u_time');
-  gl.uniform1f(loc, u_time);
-
-  loc = gl.getUniformLocation(program, 'u_duration');
-  gl.uniform1f(loc, u_duration);
-
-  loc = gl.getUniformLocation(program, 'u_dir');
-  gl.uniform2fv(loc, u_dir);
-}
-
-
-// 实现动画
-let triangles = [];
-
-function update() {
-  for(let i = 0; i < 5 * Math.random(); i++) {
-    triangles.push(randomTriangles());
-  }
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  // 对每个三角形重新设置u_time
-  triangles.forEach((triangle) => {
-    triangle.u_time = (performance.now() - triangle.startTime) / 1000;
-    setUniforms(gl, triangle);
-    gl.drawArrays(gl.TRIANGLES, 0, position.length / 2);
+// 加载图片函数
+const loadImage = (url) => {
+  const img = new Image();
+  img.src = url;
+  return new Promise((resolve) => {
+    img.onload = resolve;
   });
-  // 移除已经结束动画的三角形
-  triangles = triangles.filter((triangle) => {
-    return triangle.u_time <= triangle.u_duration;
-  });
-  requestAnimationFrame(update);
-}
+};
 
-requestAnimationFrame(update);
+const canvas = document.getElementsByTagName("canvas")[0];
+const ctx = canvas.getContext("2d");
+
+(async () => {
+  const res = await loadImage("./img/1.jpg");
+  console.log(res);
+})();
+
+//   ctx.drawImage(img, 0, 0, 64*5, 96*5)
+//   const imageData = ctx.getImageData(300, 200, 50, 50)
+//   console.log(imageData);
+
+//   // 设置新的canvas为获取的data数据
+//   const canvas2 = document.createElement('canvas')
+//   canvas2.width = 200
+//   canvas2.height = 200
+//   canvas2.getContext('2d').drawImage(imageData.data, 0, 0, 200, 200)
+//   document.body.append(canvas2)
